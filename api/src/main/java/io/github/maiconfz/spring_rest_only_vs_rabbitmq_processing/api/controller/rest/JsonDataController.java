6bf470877e1c0654713dcc9dbf05644f7e079a88
Maiconfz <maiconfz@gmail.com>
2023-09-23 11:49:49 +0100

    Remove unnecessary imports

--- api/src/main/java/io/github/maiconfz/spring_rest_only_vs_rabbitmq_processing/api/controller/rest/JsonDataController.java
+++ api/src/main/java/io/github/maiconfz/spring_rest_only_vs_rabbitmq_processing/api/controller/rest/JsonDataController.java
@@ -1,20 +1,14 @@
 package io.github.maiconfz.spring_rest_only_vs_rabbitmq_processing.api.controller.rest;
 
-import java.util.UUID;
-
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.tinylog.Logger;
-
-import com.google.common.collect.ImmutableSet;
 
 import io.github.maiconfz.spring_rest_only_vs_rabbitmq_processing.api.dto.JsonDataDto;
 import io.github.maiconfz.spring_rest_only_vs_rabbitmq_processing.api.dto.mapper.JsonDataToJsonDataDtoMapper;
