e657ef996ab82dc1461f4a165f10cd187269deb5
Maiconfz <maiconfz@gmail.com>
2024-03-08 03:08:09 +0000

    Upgrade dependencies versions

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("io.maiconfz.spring_rest_only_vs_rabbitmq_processing.java-application-conventions")
     java
-	id("org.springframework.boot") version "3.1.4"
-	id("io.spring.dependency-management") version "1.1.2"
+	id("org.springframework.boot") version "3.2.3"
+	id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "io.maiconfz.spring_rest_only_vs_rabbitmq_processing"
@@ -28,17 +28,17 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-hateoas")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	
-	implementation("com.google.guava:guava:32.1.1-jre")
+	implementation("com.google.guava:guava:33.0.0-jre")
 	
-	implementation("org.apache.commons:commons-lang3:3.13.0")
+	implementation("org.apache.commons:commons-lang3:3.14.0")
 	
-	implementation("org.tinylog:tinylog-api:2.6.2")
-	implementation("org.tinylog:tinylog-impl:2.6.2")
+	implementation("org.tinylog:tinylog-api:2.7.0")
+	implementation("org.tinylog:tinylog-impl:2.7.0")
 	
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 
-	implementation("org.postgresql:postgresql:42.6.0")
+	implementation("org.postgresql:postgresql:42.7.2")
 
 	compileOnly("org.projectlombok:lombok")
 
