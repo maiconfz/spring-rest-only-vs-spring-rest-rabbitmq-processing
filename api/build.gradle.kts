ddf847c49e5d6ffac0bdb2d2597e89575f063e32
Maiconfz <maiconfz@gmail.com>
2024-11-30 22:15:50 +0000

    Upgrade depencies versions

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("io.maiconfz.spring_rest_only_vs_rabbitmq_processing.java-application-conventions")
     java
-	id("org.springframework.boot") version "3.3.3"
+	id("org.springframework.boot") version "3.4.0"
 	id("io.spring.dependency-management") version "1.1.6"
 }
 
@@ -28,27 +28,28 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-hateoas")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	
-	implementation("com.google.guava:guava:33.3.0-jre")
+	implementation("com.google.guava:guava:33.3.1-jre")
 	
-	implementation("org.apache.commons:commons-lang3:3.16.0")
+	implementation("org.apache.commons:commons-lang3:3.17.0")
 	
 	implementation("org.tinylog:tinylog-api:2.7.0")
 	implementation("org.tinylog:tinylog-impl:2.7.0")
 	
-	implementation("org.mapstruct:mapstruct:1.6.0")
+	implementation("org.mapstruct:mapstruct:1.6.3")
 
-	implementation("org.postgresql:postgresql:42.7.2")
+	implementation("org.postgresql:postgresql:42.7.4")
 
-	compileOnly("org.projectlombok:lombok:1.18.34")
+	compileOnly("org.projectlombok:lombok:1.18.36")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	developmentOnly("org.springframework.data:spring-data-rest-hal-explorer")
 
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-	annotationProcessor("org.projectlombok:lombok:1.18.34")
-	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0")
+	annotationProcessor("org.projectlombok:lombok:1.18.36")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.amqp:spring-rabbit-test")	
